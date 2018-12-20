8b32c30d3152be1e86c1706e255e88b641215302
Nuttawut <slothslower@Nuttawuts-MacBook-Pro.local>
2018-12-20 15:52:43 +0700

    final

--- base/urls.py
+++ base/urls.py
@@ -6,11 +6,6 @@ app_name = 'base'
 urlpatterns = [
 	
 	url(r'^$', views.index, name='index'),
-	url(r'^about/$', views.about, name='about'),
-	url(r'^contactus/$', views.contactus, name='contactus'),
-	url(r'^service/$', views.service, name='service'),
-	url(r'^event/$', views.event, name='event'),
-	url(r'^property/$', views.property, name='property'),
 	url(r'^activity/$', views.activity, name='activity'),
 	
 ]
